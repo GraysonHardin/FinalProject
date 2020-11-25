5c76e5667975bea2d3401bd46f0cffa44f38d37e
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-11-25 10:59:01 -0600

    Beginning stages of project; CSV added.

---
+++ main/vehicle_demos.py
@@ -0,0 +1,8 @@
+class VehicleDemos:
+    def __init__(self, make, model, year, mileage, price, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.mileage = mileage
+        self.price = price
+        self.color = color
