725974bf19e2de8faed7d33c5eb7da1832217080
Grayson Hardin <grayhardin@gmail.com>
2020-11-27 11:02:23 -0600

    Added more features

--- main/gui_manager.py
+++ main/gui_manager.py
@@ -2,9 +2,8 @@ import tkinter
 import tkinter as tk
 from main.csv_manager.catalog import search
 
-
 m = tkinter.Tk()
-m.geometry('1280x720')
+m.geometry('1920x1080')
 m.title('Catalog')
 
 # make input
@@ -14,58 +13,41 @@ m.title('Catalog')
 # table grid to show results
 
 
-make_input = tk.Entry(m, width=70)
-make_input.grid(row=2)
+make_input = tk.Entry(m, width=25)
+make_input.grid(row=1)
 
+model_input = tk.Entry(m, width=25)
+model_input.grid(row=2)
 
-model_input = tk.Entry(m, width=70)
-model_input.grid(row=3)
-
-year_input = tk.Entry(m, width=70)
-year_input.grid(row=4)
+year_input = tk.Entry(m, width=25)
+year_input.grid(row=3)
 
 
 def search_catalog():
     search_results = search(make_input.get(), model_input.get(), year_input.get())
     print(search_results)
+    draw_table(search_results)
+    model_input.delete(0, tk.END)
+    make_input.delete(0, tk.END)
+    year_input.delete(0, tk.END)
 
-    draw_table(search_results, 2)
 
-
-def draw_table(rows, grid_index):
+def draw_table(rows):
     for i in range(len(rows)):
-        e = tkinter.Entry(m, width=20, fg='black', font=('Arial', 12, 'bold'))
+        column = 1
+        for key in rows[i]:
+            e = tkinter.Entry(m, width=20, fg='black', font=('Arial', 12, 'bold'))
 
-        e.grid(row=i, column=2)
-        e.insert(tkinter.END, rows[i]['make'])
-
-        model = tkinter.Entry(m, width=20, fg='black', font=('Arial', 12, 'bold'))
-        model.grid(row=i, column=3)
-        model.insert(tkinter.END, rows[i]['model'])
-
-    # for i in range(len(rows)):
-    #     for j in range(len(rows)):
-    #         e = tkinter.Entry(m, width=20, fg='black', font=('Arial', 12, 'bold'))
-    #         e.grid(row=i, column=j + grid_index)
-    #         e.insert(tkinter.END, rows[i]['make'])
+            e.grid(row=i, column=column)
+            e.insert(tkinter.END, rows[i][key])
+            column += 1
+            e['state'] = tkinter.DISABLED
 
 
+create_search_button = tkinter.Button(m, text='Search Catalog', width=25, command=search_catalog)
+create_search_button.grid(row=0)
 
-
-    # for i in range(total_rows):
-    #     for j in range(total_columns):R
-    #         e = tkinter.Entry(m, width=20, fg='black', font=('Arial', 12, 'bold'))
-    #
-    #         e.grid(row=i, column=j + column_index)
-    #
-    #         e.insert(tkinter.END, rows[i][j])
-    #         e['state'] = tkinter.DISABLED
-
-
-create_search_button = tkinter.Button(m, text='Search Catalog', width=60, command=search_catalog)
-create_search_button.grid(row=1)
-
-exit_button = tkinter.Button(m, text='Exit', width=60, command=m.destroy)
+exit_button = tkinter.Button(m, text='Exit', width=25, command=m.destroy)
 exit_button.grid(row=10)
 
 m.mainloop()
