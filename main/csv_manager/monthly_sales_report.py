81ff3e0a0bf35d0c0d8676e5bd53b41effbed374
Grayson Hardin <grayhardin@gmail.com>
2020-12-08 07:27:27 -0600

    Test monthly sales report

--- main/csv_manager/monthly_sales_report.py
+++ main/csv_manager/monthly_sales_report.py
@@ -5,6 +5,7 @@ def monthly_sales_report(rows):
     employee_writer = csv_writer()
 
     employee_writer.writerow(['Vehicle ID', 'Make', 'Model', 'Year', 'Purchase Price', 'Sold Price'])
-    # for row in rows:
-    #     employee_writer.writerow(
-    #         [row['id'], row['make'], row['model'], row['year'], row['purchase_price'], row['sell_price']])
+
+    for row in rows:
+        employee_writer.writerow(
+            [row['id'], row['make'], row['model'], row['year'], row['purchase_price'], row['sell_price']])
--- tests/test_monthly_sales_report.py
+++ tests/test_monthly_sales_report.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, call, MagicMock
 
 from main.csv_manager.monthly_sales_report import monthly_sales_report
 
@@ -18,11 +18,26 @@ class MyTestCase(unittest.TestCase):
             'Sold Price'
         ])
 
-    def test_csv_rows(self):
-        rows = [{'id': '1', 'make': 'Ford'}]
+    @patch('main.csv_manager.monthly_sales_report.csv_writer')
+    def test_csv_rows(self, mock_csv_writer):
+        mock_csv_writer.return_value = MagicMock()
+        rows = [{
+            'id': '1',
+            'make': 'Ford',
+            'model': 'Mustang',
+            'year': '2016',
+            'purchase_price': '20000',
+            'sell_price': '30000'
+        }]
 
-        monthly_sales_report(rows=[])
-        pass
+        monthly_sales_report(rows=rows)
+
+        calls = [
+            call(['Vehicle ID', 'Make', 'Model', 'Year', 'Purchase Price', 'Sold Price']),
+            call(['1', 'Ford', 'Mustang', '2016', '20000', '30000'])
+        ]
+
+        mock_csv_writer.return_value.writerow.assert_has_calls(calls, any_order=False)
 
 
 if __name__ == '__main__':
